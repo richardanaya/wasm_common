e1f3fb09045ffd3c90b36f7bbb77d9132f36d285
richard <richard>
2019-11-13 22:06:25 -0800

    inline

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,12 @@ pub const TYPE_BUI64_ARRAY: JSType = 16;
 pub const TYPE_MEMORY: JSType = 17;
 
 pub trait ToJSValue {
+    #[inline(Always)]
     fn to_js_value(&self) -> JSValue;
 }
 
 impl ToJSValue for JSValue {
+    #[inline]
     fn to_js_value(&self) -> JSValue {
         *self as JSValue
     }
